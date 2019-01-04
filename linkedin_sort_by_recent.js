bf291af9cf3366cb06caa20f616dfacfa7e0222d
jcr0ss <31080060+jcr0ss@users.noreply.github.com>
2019-01-04 00:14:34 -0500

    Update linkedin_sort_by_recent.js

--- linkedin_sort_by_recent.js
+++ linkedin_sort_by_recent.js
@@ -1,14 +1,14 @@
 // Using this chrome extension, run custom JS on websites. In this case for LinkedIn.
 // https://chrome.google.com/webstore/detail/user-javascript-and-css/nbhcbdghjpllgmfilhnhkllmkecfmpld?hl=en
 $(document).ready(function() {
-setTimeout(function() {
-    $('button[class*=sort-dropdown__dropdown-trigger]').click();
     setTimeout(function() {
-        $('li[class=sort-dropdown__list-item] button').each(function(row) {
-            if ($(this).text().indexOf("Recent") >= 0) {
-                $(this).click();
-            }
-        });
-    }, 500);
-}, 2000);
+        $('button[class*=sort-dropdown__dropdown-trigger]').click();
+        setTimeout(function() {
+            $('li[class=sort-dropdown__list-item] button').each(function(row) {
+                if ($(this).text().indexOf("Recent") >= 0) {
+                    $(this).click();
+                }
+            });
+        }, 500);
+    }, 2000);
 });
