95f8f0f0a67354485015dbdbf55848e14242134a
jcr0ss <31080060+jcr0ss@users.noreply.github.com>
2019-03-12 19:20:20 -0400

    Update stocktwits_popup_remover.js

--- stocktwits_popup_remover.js
+++ stocktwits_popup_remover.js
@@ -1,5 +1,5 @@
 // use this extension: https://chrome.google.com/webstore/detail/user-javascript-and-css/nbhcbdghjpllgmfilhnhkllmkecfmpld?hl=en
-// go to stocktwits.com. click on the extension icon. click the add new button. paste this. reload stocktwits. 
+// go to stocktwits.com. click on the extension icon. click the 'add new' button. paste this code. reload stocktwits. 
 // make sure it's enabled by clicking the extension icon again, and seeing that the selector to the left of stocktwits is green.
 $(document).ready(function() {
     for (var i = 0; i < 20; i++) {
